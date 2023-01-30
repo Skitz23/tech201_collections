3a0ad944c131e8401726ddd4c53a76e5299f1c7d
Zain Badshah <zainbadshah786@hotmail.com>
2023-01-30 18:04:18 +0000

    food menu

---
+++ Food_menu.py
@@ -0,0 +1,11 @@
+print("Good Evening what would you like to order.")
+
+Starter = ["Garlic bread", "Hummouse"]
+mains = ["Kung po chicken", "Spicy beef", "Toasted prawns" ]
+Desserts = ["Banana toffee pudding", "Glazed donuts", "Vanilla ice cream" ]
+Drinks = ["Pepsi", "Fanta" ]
+
+customers = input()
+print(f" What would you like to eat today? {customers}?")#
+custumor = input()
+print("Your food shall be with you in 20 minuites.")
