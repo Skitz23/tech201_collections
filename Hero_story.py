68038db7aecc5ded49912d21778eaf1d57b920c3
Zain Badshah <zainbadshah786@hotmail.com>
2023-01-30 17:56:12 +0000

    Story

---
+++ Hero_story.py
@@ -0,0 +1,16 @@
+story_1= {
+    "Beginning": " Once upon a time their was a hero called Billy. Billy had a problem that he had to win the battle and the only way he could do that was if he discovered a super power",
+    "Middle": "Billy discovered that he had plants growing in his garden and the plant would make him breath fire",
+    "End": "Billy grew the plant and he ate them so much that it made him breath so much fire and we won the battle"
+}
+print(story_1)
+print(type(story_1))
+print(story_1.keys())
+print(story_1.values())
+print(story_1["Beginning"])
+print(story_1["Middle"])
+print(story_1["End"])
+
+story_1["SuperHero"] = "Billy"
+print(story_1)
+
